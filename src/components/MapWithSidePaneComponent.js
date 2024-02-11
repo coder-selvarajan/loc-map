77ed587bb3c54684eb7c9503a36e0931cca25927
Selvarajan Thangavel <78447+coder-selvarajan@users.noreply.github.com>
2024-02-12 00:29:26 +0530

    Optimised for mobile view

--- src/components/MapWithSidePaneComponent.js
+++ src/components/MapWithSidePaneComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -6,7 +6,6 @@ import { FaLocationDot, FaIndianRupeeSign, FaLink } from "react-icons/fa6";
 import { BsImage } from "react-icons/bs";
 import YoutubeEmbed from "./YoutubeEmbed";
 import data from '../resources/location-data.json';
-
 
 // Fixing an issue with the default marker icons
 delete L.Icon.Default.prototype._getIconUrl;
@@ -30,25 +29,32 @@ const createCustomMarkerIcon = (name, isSelected) => new L.DivIcon({
 
 const MapSidePaneComponent = () => {
   const [selectedLocation, setSelectedLocation] = useState(data.locations[0]); // State to hold the selected location
-  // const [isPopupVisible, setIsPopupVisible] = useState(false); // State for controlling popup visibility
+  const [isMobileView, setIsMobileView] = useState(window.innerWidth < 768); // Added to detect mobile view
+  const [isPopupVisible, setIsPopupVisible] = useState(false); // Added to control popup visibility on mobile
+
+  // Update isMobileView based on window width
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   const mapRef = useRef(null);
   
-  // Check if the screen is mobile size
-  // const isMobile = window.innerWidth < 768;
-
   // Side Pane Content Component
   const SidePane = ({ location, onClose }) => {
     if (!location) return null;
 
-    // if (!location) return <div className="side-pane" style={{ width: '300px', height: '100vh', position: 'absolute', top: '0', right: '0', background: 'whitesmoke', opacity: 0.5, borderLeft: '1px grey solid', padding: '20px', boxSizing: 'border-box' }}>Please select a location <br/>to view the details here.</div>;
+    const handleClose = () => {
+      setIsPopupVisible(false); // Hide the popup when close button is clicked
+    };
 
     return (
-      
-      // <div className={`side-pane ${isMobile ? 'mobile-popup' : ''}`} style={{ width: '300px', height: '100vh', position: 'absolute', top: '0', right: isMobile ? '0' : 'initial', background: 'whitesmoke', padding: '20px', borderLeft: '1px grey solid', boxSizing: 'border-box', textAlign: "left", zIndex: 1000 }}>
-      //   {isMobile && <button onClick={onClose} style={{ position: 'absolute', top: '10px', right: '10px', background: 'transparent', border: 'none', fontSize: '24px' }}><FaTimes /></button>}
-      //   <h2>{location.name}</h2>
-
-      <div className="side-pane" style={{ width: '300px', height: '100vh', position: 'absolute', top: '0', right: '0', background: 'whitesmoke', padding: '20px', borderLeft: '1px grey solid', boxSizing: 'border-box', textAlign: "left" }}>
+      <div className={`side-pane ${isMobileView && isPopupVisible ? 'mobile-popup' : ''}`} >
+        {isMobileView && <button onClick={handleClose} style={{ position: 'absolute', top: '10px', right: '10px' }}>Close</button>}
         <h2>{location.name}</h2>
         
         <p> <FaLocationDot /> &nbsp; {location.location}</p>
@@ -88,16 +94,9 @@ const MapSidePaneComponent = () => {
     );
   };
 
-  // Open popup when a location is selected on mobile
-  // useEffect(() => {
-  //   if (isMobile) setIsPopupVisible(true);
-  // }, [selectedLocation, isMobile]);
-
   return (
     <>
-      {/* <MapContainer center={[11.09491, 77.04082]} zoom={13.5} style={{ height: '100vh', width: isMobile ? '100%' : 'calc(100% - 300px)' }}
-          whenCreated={mapInstance => { mapRef.current = mapInstance; }}> */}
-      <MapContainer center={[11.09491, 77.04082]} zoom={12.5} style={{ height: '100vh', width: 'calc(100% - 300px)' }}
+     <MapContainer center={[11.09491, 77.04082]} zoom={12.5} style={{ height: '100vh', width: '100%' }}
         whenCreated={mapInstance => { mapRef.current = mapInstance; }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -109,15 +108,15 @@ const MapSidePaneComponent = () => {
                   eventHandlers={{
                     click: () => {
                       setSelectedLocation(location);
-                      
+                      if (isMobileView) {
+                        setIsPopupVisible(true)
+                      }
                     },
                   }}>
           </Marker>
           
         ))}
       </MapContainer>
-      {/* {(isMobile && isPopupVisible) && <SidePane location={selectedLocation} onClose={() => setIsPopupVisible(false)} />}
-      {!isMobile && <SidePane location={selectedLocation} />} */}
 
       <SidePane location={selectedLocation} />
     </>
@@ -125,5 +124,3 @@ const MapSidePaneComponent = () => {
 };
 
 export default MapSidePaneComponent;
-
-
