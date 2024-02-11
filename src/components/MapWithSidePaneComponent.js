24dd24eaea60d6683714cc8e9de453ddce10925d
Selvarajan Thangavel <selvarajan2006@gmail.com>
2024-02-11 21:42:42 +0530

    mobile view popup configs

--- src/components/MapWithSidePaneComponent.js
+++ src/components/MapWithSidePaneComponent.js
@@ -1,11 +1,13 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { FaLocationDot, FaIndianRupeeSign, FaLink } from "react-icons/fa6";
+import { FaTimes } from "react-icons/fa"; // Include FaTimes for the close button
 import { BsImage } from "react-icons/bs";
 import YoutubeEmbed from "./YoutubeEmbed";
-import data from '../resources/location-data.json'; // Ensure this import path is correct
+import data from '../resources/location-data.json';
+
 
 // Fixing an issue with the default marker icons
 delete L.Icon.Default.prototype._getIconUrl;
@@ -16,14 +18,8 @@ L.Icon.Default.mergeOptions({
 });
 
 const createCustomMarkerIcon = (name, isSelected) => new L.DivIcon({
-  // html: `
-  //   <div style="display: flex; align-items: center;">
-  //     <img src="${L.Icon.Default.prototype.options.iconUrl}" style="margin-right: 4px;"/>
-  //     <span style="font-size: ${textSize}px; font-weight: bold; text-align: left;">${name}</span>
-  //   </div>
-  // `,
   html: `
-    <div style="display: flex; align-items: center; background-color: ${isSelected ? '#ffdfba' : 'transparent'}; padding: 4px; border-radius: 8px;">
+    <div style="display: flex; align-items: center; padding: 4px; border-radius: 8px;">
       <img src="${L.Icon.Default.prototype.options.iconUrl}" style="margin-right: 4px;"/>
       <span style="font-size: ${isSelected ? '12' : '11'}px; color: ${isSelected ? 'red' : 'black'}; font-weight: bold; text-align: left;">${name}</span>
     </div>
@@ -35,14 +31,24 @@ const createCustomMarkerIcon = (name, isSelected) => new L.DivIcon({
 
 const MapSidePaneComponent = () => {
   const [selectedLocation, setSelectedLocation] = useState(data.locations[0]); // State to hold the selected location
+  // const [isPopupVisible, setIsPopupVisible] = useState(false); // State for controlling popup visibility
   const mapRef = useRef(null);
   
+  // Check if the screen is mobile size
+  // const isMobile = window.innerWidth < 768;
 
   // Side Pane Content Component
-  const SidePane = ({ location }) => {
-    if (!location) return <div className="side-pane" style={{ width: '300px', height: '100vh', position: 'absolute', top: '0', right: '0', background: 'whitesmoke', opacity: 0.5, borderLeft: '1px grey solid', padding: '20px', boxSizing: 'border-box' }}>Please select a location <br/>to view the details here.</div>;
+  const SidePane = ({ location, onClose }) => {
+    if (!location) return null;
+
+    // if (!location) return <div className="side-pane" style={{ width: '300px', height: '100vh', position: 'absolute', top: '0', right: '0', background: 'whitesmoke', opacity: 0.5, borderLeft: '1px grey solid', padding: '20px', boxSizing: 'border-box' }}>Please select a location <br/>to view the details here.</div>;
 
     return (
+      
+      // <div className={`side-pane ${isMobile ? 'mobile-popup' : ''}`} style={{ width: '300px', height: '100vh', position: 'absolute', top: '0', right: isMobile ? '0' : 'initial', background: 'whitesmoke', padding: '20px', borderLeft: '1px grey solid', boxSizing: 'border-box', textAlign: "left", zIndex: 1000 }}>
+      //   {isMobile && <button onClick={onClose} style={{ position: 'absolute', top: '10px', right: '10px', background: 'transparent', border: 'none', fontSize: '24px' }}><FaTimes /></button>}
+      //   <h2>{location.name}</h2>
+
       <div className="side-pane" style={{ width: '300px', height: '100vh', position: 'absolute', top: '0', right: '0', background: 'whitesmoke', padding: '20px', borderLeft: '1px grey solid', boxSizing: 'border-box', textAlign: "left" }}>
         <h2>{location.name}</h2>
         
@@ -83,9 +89,15 @@ const MapSidePaneComponent = () => {
     );
   };
 
+  // Open popup when a location is selected on mobile
+  useEffect(() => {
+    if (isMobile) setIsPopupVisible(true);
+  }, [selectedLocation, isMobile]);
 
   return (
     <>
+      {/* <MapContainer center={[11.09491, 77.04082]} zoom={13.5} style={{ height: '100vh', width: isMobile ? '100%' : 'calc(100% - 300px)' }}
+          whenCreated={mapInstance => { mapRef.current = mapInstance; }}> */}
       <MapContainer center={[11.09491, 77.04082]} zoom={13.5} style={{ height: '100vh', width: 'calc(100% - 300px)' }}
         whenCreated={mapInstance => { mapRef.current = mapInstance; }}>
         <TileLayer
@@ -93,14 +105,6 @@ const MapSidePaneComponent = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         {data.locations.map((location, index) => (
-          // <Marker key={index} position={[location.lat, location.lng]} 
-          //         icon={createCustomMarkerIcon(location.display, textSize)}
-          //         eventHandlers={{
-          //           click: () => {
-          //             setSelectedLocation(location);
-          //           },
-          //         }}>
-          // </Marker>
           <Marker key={index} position={[location.lat, location.lng]} 
                   icon={createCustomMarkerIcon(location.display, selectedLocation === location)}
                   eventHandlers={{
@@ -113,6 +117,9 @@ const MapSidePaneComponent = () => {
           
         ))}
       </MapContainer>
+      {/* {(isMobile && isPopupVisible) && <SidePane location={selectedLocation} onClose={() => setIsPopupVisible(false)} />}
+      {!isMobile && <SidePane location={selectedLocation} />} */}
+
       <SidePane location={selectedLocation} />
     </>
   );
